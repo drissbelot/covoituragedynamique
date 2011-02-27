690fc5f1aac94451d86126584fe170e34c1b73b7
joachim.beilharz <joachim.beilharz@2af29f76-a27f-8fc1-9967-9f9aca168193>
2011-02-27 10:47:20 +0000

    itinéraire

--- Covoiturage/src/com/covoiturage/client/presenter/MapPresenter.java
+++ Covoiturage/src/com/covoiturage/client/presenter/MapPresenter.java
@@ -1,15 +1,19 @@
 package com.covoiturage.client.presenter;
 
-import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.event.dom.client.HasClickHandlers;
 import com.google.gwt.event.shared.HandlerManager;
 import com.google.gwt.maps.client.MapWidget;
+import com.google.gwt.maps.client.geocode.DirectionQueryOptions;
+import com.google.gwt.maps.client.geocode.DirectionResults;
+import com.google.gwt.maps.client.geocode.Directions;
+import com.google.gwt.maps.client.geocode.DirectionsCallback;
+import com.google.gwt.maps.client.geocode.DirectionsPanel;
 import com.google.gwt.maps.client.geocode.Geocoder;
 import com.google.gwt.maps.client.geocode.LatLngCallback;
+import com.google.gwt.maps.client.geocode.StatusCodes;
 import com.google.gwt.maps.client.geom.LatLng;
-import com.google.gwt.maps.client.overlay.Marker;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.HasWidgets;
 import com.google.gwt.user.client.ui.Widget;
@@ -18,69 +22,109 @@ public class MapPresenter implements Presenter {
 
 
 
-  public interface Display {
-   
+	public interface Display {
 
 
-    Widget asWidget();
 
-	HasClickHandlers getSendAddressButton();
+		Widget asWidget();
 
-	String getSendAddress();
+		HasClickHandlers getSendAddressButton();
 
-	MapWidget getMap();
-  }
-  
- // private final MapServiceAsync rpcService;
-  private final HandlerManager eventBus;
-  private final Display display;
-  private Geocoder geocoder;
-  private String address;
-  
-  public MapPresenter(/*ContactsServiceAsync rpcService, */HandlerManager eventBus, Display view) {
-  //  this.rpcService = rpcService;
-    this.eventBus = eventBus;
-    this.display = view;
-  }
-  
-  public void bind() {
+		String getDestinationAddress();
+
+		MapWidget getMap();
+
+		String getOriginAddress();
+
+		DirectionsPanel getDirectionsPanel();
+	}
+
+	// private final MapServiceAsync rpcService;
+	private final HandlerManager eventBus;
+	private final Display display;
+	private Geocoder geocoder;
+	private String addressOrigin;
+	private String addressDestination;
+
+
+	public MapPresenter(/*ContactsServiceAsync rpcService, */HandlerManager eventBus, Display view) {
+		//  this.rpcService = rpcService;
+		this.eventBus = eventBus;
+		this.display = view;
+	}
+
+	public void bind() {
 		display.getSendAddressButton().addClickHandler(new ClickHandler() {   
-		      public void onClick(ClickEvent event) {
-		        doGeolocate();
-		      }
-		    });
-	  
-	  
-  }
-  
-  
-  public void go(final HasWidgets container) {
-	    bind();
-	    container.clear();
-	    container.add(display.asWidget());
+			public void onClick(ClickEvent event) {
+				clearGeolocate();
+				doGeolocate();
+			}
+		});
 
-	  }
-  private void doGeolocate() {
-	  geocoder= new Geocoder();
-	  address =display.getSendAddress();
 
-	  geocoder.getLatLng(address, new LatLngCallback() {
-	      public void onFailure() {
-	          Window.alert(address + " not found");
-	        }
+	}
 
-	        public void onSuccess(LatLng point) {
-	          display.getMap().setCenter(point, 13);
-	          Marker marker = new Marker(point);
-	          display.getMap().addOverlay(marker);
-	          
-	          //displayLatLng(point);
-	        }
-	      });
 
-	
+	public void go(final HasWidgets container) {
+		bind();
+		container.clear();
+		container.add(display.asWidget());
+
+	}
+	private void clearGeolocate(){
+		
+		display.getMap().clearOverlays();
+		
 	}
 	
-	  
-  
+	
+	private void doGeolocate() {
+		geocoder= new Geocoder();
+		addressOrigin =display.getOriginAddress();
+		addressDestination = display.getDestinationAddress();
+
+	
+		DirectionQueryOptions opts = new DirectionQueryOptions(display.getMap(),display.getDirectionsPanel());
+
+		geocoder.getLatLng(addressOrigin, new LatLngCallback() {
+			public void onFailure() {
+				Window.alert(addressOrigin + " not found");
+			}
+
+			public void onSuccess(LatLng point) {
+
+
+
+			}
+		});
+		geocoder.getLatLng(addressDestination, new LatLngCallback() {
+			public void onFailure() {
+				Window.alert(addressDestination + " not found");
+			}
+
+			public void onSuccess(LatLng point) {
+
+
+
+			}
+		});
+
+		Directions.load("from: "+addressOrigin +" to: "+ addressDestination, opts,  new DirectionsCallback() {
+			public void onFailure(int statusCode) {
+				Window.alert("Failed to load directions: Status "
+						+ StatusCodes.getName(statusCode) + " " + statusCode);
+			}
+
+			public void onSuccess(DirectionResults result) {
+
+			}
+		});
+
+
+
+
+	}
+
+
+
 }
--- Covoiturage/src/com/covoiturage/client/view/MapView.java
+++ Covoiturage/src/com/covoiturage/client/view/MapView.java
@@ -3,6 +3,7 @@ import com.covoiturage.client.presenter.MapPresenter;
 import com.google.gwt.event.dom.client.HasClickHandlers;
 import com.google.gwt.maps.client.MapWidget;
 import com.google.gwt.maps.client.control.LargeMapControl;
+import com.google.gwt.maps.client.geocode.DirectionsPanel;
 import com.google.gwt.maps.client.geom.LatLng;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Composite;
@@ -11,6 +12,7 @@ import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.Panel;
 import com.google.gwt.user.client.ui.TextBox;
+import com.google.gwt.user.client.ui.VerticalPanel;
 
 public class MapView extends Composite implements MapPresenter.Display {
 
@@ -20,7 +22,10 @@ public class MapView extends Composite implements MapPresenter.Display {
 	  private FlowPanel mapDecorator;
 	  private MapWidget mapWidget;
 	  private Button sendAddress;
-	  private  TextBox address;
+	  private  TextBox originAddress;
+	  private  TextBox destinationAddress;
+	  private VerticalPanel addressPanel;
+	  private DirectionsPanel directionsPanel;
 
 	public MapView() {
 		mapDecorator = new FlowPanel();
@@ -28,19 +33,28 @@ public class MapView extends Composite implements MapPresenter.Display {
 		 mapWidget = new MapWidget(LatLng.newInstance(48.136559, 11.576318), 13);
 	     mapWidget.setSize("500px", "500px");
 	     mapWidget.addControl(new LargeMapControl());
-	     
-	        address= new TextBox();
-	        address.setVisibleLength(60);
-	        address.setText("1 Grand Place, Louvain-la-neuve");
+	    addressPanel = new VerticalPanel();
+	    
+	        originAddress= new TextBox();
+	        originAddress.setVisibleLength(60);
+	        originAddress.setText("1 Grand Place, Louvain-la-neuve");
+	        destinationAddress= new TextBox();
+	        destinationAddress.setVisibleLength(60);
+	        destinationAddress.setText("1 Grand Place, Louvain-la-neuve");
 	        sendAddress= new Button("Localiser");
 	        
+	         directionsPanel = new DirectionsPanel();
+
 	        
-	        
-	        mapDecorator.add(address);
-	        mapDecorator.add(sendAddress);
+	        addressPanel.add(originAddress);
+	        addressPanel.add(destinationAddress);
+	        addressPanel.add(sendAddress);
+
 	        mapDecorator.add(buildLatLngPanel());
 	        
 	        mapDecorator.add(mapWidget);
+	        mapDecorator.add(addressPanel);
+	        mapDecorator.add(directionsPanel);
 	          }
 	
 	 public HasClickHandlers getSendAddressButton() {
@@ -62,15 +76,26 @@ public class MapView extends Composite implements MapPresenter.Display {
 	}
 
 	@Override
-	public String getSendAddress() {
+	public String getOriginAddress() {
 		
-		return address.getText();
+		return originAddress.getText();
 	}
+public String getDestinationAddress() {
+		
+		return destinationAddress.getText();
+	}
+	
 
 	@Override
 	public MapWidget getMap() {
 
 		return mapWidget;
+	}
+
+	@Override
+	public DirectionsPanel getDirectionsPanel() {
+		
+		return directionsPanel;
 	}
 	        
 }

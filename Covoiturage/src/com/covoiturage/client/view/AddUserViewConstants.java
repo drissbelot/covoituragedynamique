1283ebb5e6421483aa0974d3f583cecd1e2c59cd
projet.sinf.2011 <projet.sinf.2011@2af29f76-a27f-8fc1-9967-9f9aca168193>
2011-03-29 20:44:48 +0000

    part 3 - the end

---
+++ Covoiturage/src/com/covoiturage/client/view/AddUserViewConstants.java
@@ -0,0 +1,23 @@
+package com.covoiturage.client.view;
+
+import com.google.gwt.i18n.client.Constants;
+
+public interface AddUserViewConstants extends Constants{
+
+	String header();
+	String username();
+	String firstname();
+	String lastname();
+	String email();
+	String passwordlab();
+	String fr();
+	String it();
+	String en();
+	String ch();
+	String nl();
+	String languelab();
+	String add();
+	String make();
+	String model();
+	
+}
--- Covoiturage/src/com/covoiturage/client/view/AddUserViewImpl.java
+++ Covoiturage/src/com/covoiturage/client/view/AddUserViewImpl.java
@@ -7,6 +7,7 @@ import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.FlowPanel;
+import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.ListBox;
 import com.google.gwt.user.client.ui.PasswordTextBox;
 import com.google.gwt.user.client.ui.SuggestBox;
@@ -17,27 +18,40 @@ public class AddUserViewImpl extends Composite implements  AddUserView {
 	
 	interface MyUiBinder extends UiBinder<FlowPanel, AddUserViewImpl> { }
     private static final MyUiBinder binder = GWT.create(MyUiBinder.class);
-	
+    private AddUserViewConstants constants=(AddUserViewConstants)GWT.create(AddUserViewConstants.class);
+    
 	@UiField FlowPanel flowpanel;
 	@UiField Button addButton;
-	@UiField TextBox firstName;
-	@UiField TextBox lastName;
-	@UiField TextBox emailAdress;
-	@UiField TextBox login;
+	@UiField TextBox firstName,lastName,emailAdress,login;
 	@UiField PasswordTextBox password;
-	@UiField SuggestBox vehicleMake;
-	@UiField SuggestBox vehicleModel;
-	@UiField ListBox language;
+	@UiField SuggestBox vehicleMake,vehicleModel;
+	@UiField ListBox langue;
+	@UiField Label header,username,lastname,firstname,email,passwordlab,languelab,Make,Model;
 
 	public AddUserViewImpl() {
 		
 		initWidget(binder.createAndBindUi(this)); 
 		
-		language.addItem("Francais");
-		language.addItem("Anglais");
-		language.addItem("Néerlandais");
-		language.addItem("Italien");
-		language.addItem("Chinois");
+		header.setText(constants.header()+" :");
+		login.setText(constants.header());
+		username.setText(constants.username()+" :");
+		lastname.setText(constants.lastname()+" :");
+		lastName.setText(constants.lastname());
+		firstname.setText(constants.firstname()+" :");
+		firstName.setText(constants.firstname());
+		email.setText(constants.email()+" :");
+		emailAdress.setText(constants.email());
+		passwordlab.setText(constants.passwordlab()+" :");
+		password.setText(constants.passwordlab());
+		languelab.setText(constants.languelab()+" :");
+		Make.setText(constants.make());
+		Model.setText(constants.model());
+		langue.addItem(constants.fr());
+		langue.addItem(constants.en());
+		langue.addItem(constants.nl());
+		langue.addItem(constants.it());
+		langue.addItem(constants.ch());
+		addButton.setText(constants.add());
 	}
 	
 	
@@ -86,10 +100,9 @@ public class AddUserViewImpl extends Composite implements  AddUserView {
 		return vehicleModel;
 	}
 
-
 	@Override
 	public ListBox getLanguage() {
 
-		return language;
+		return langue;
 	}
 }

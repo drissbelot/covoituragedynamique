108f4c61a6c19ba0efc2268f37844c72eab803ce
projet.sinf.2011 <projet.sinf.2011@2af29f76-a27f-8fc1-9967-9f9aca168193>
2011-03-29 07:20:52 +0000

    Junit - le début

---
+++ Covoiturage/test/com/covoiturage/client/test1.java
@@ -0,0 +1,16 @@
+package com.covoiturage.client;
+
+import com.google.gwt.junit.client.GWTTestCase;
+
+public class Test1 extends GWTTestCase{
+
+	public String getModuleName() {
+		return "com.covoiturage.client";
+	}
+
+	public void testSimple() {
+		assertTrue(true);
+	}
+
+
+}

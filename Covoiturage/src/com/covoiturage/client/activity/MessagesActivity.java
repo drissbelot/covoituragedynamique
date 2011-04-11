d278264da06babd71a44aaef6c40131a47b1fc88
apsforps <apsforps@gmail.com>
2011-04-11 06:51:34 +0000

    messages (5)

--- Covoiturage/src/com/covoiturage/client/activity/MessagesActivity.java
+++ Covoiturage/src/com/covoiturage/client/activity/MessagesActivity.java
@@ -1,13 +1,26 @@
 package com.covoiturage.client.activity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.covoiturage.client.ClientFactory;
 
+import com.covoiturage.client.event.SendLoginEvent;
+import com.covoiturage.client.event.SendLoginEventHandler;
 import com.covoiturage.client.view.MessagesView;
 import com.covoiturage.shared.CovoiturageRequestFactory;
+import com.covoiturage.shared.MessagesProxy;
+import com.covoiturage.shared.MessagesRequest;
+import com.covoiturage.shared.UserInfoDetailsProxy;
+import com.covoiturage.shared.UserInfoDetailsRequest;
+import com.covoiturage.shared.UserInfoProxy;
+
 import com.google.gwt.activity.shared.AbstractActivity;
 import com.google.gwt.event.shared.EventBus;
 import com.google.gwt.place.shared.Place;
 import com.google.gwt.place.shared.PlaceController;
+import com.google.gwt.requestfactory.shared.Receiver;
+import com.google.gwt.requestfactory.shared.Request;
 import com.google.gwt.user.client.ui.AcceptsOneWidget;
 
 public class MessagesActivity extends AbstractActivity implements MessagesView.Presenter{
@@ -15,7 +28,12 @@ public class MessagesActivity extends AbstractActivity implements MessagesView.P
 	private final MessagesView messageView;
 	private CovoiturageRequestFactory requestFactory;
 	private PlaceController placeController;
+
+	private UserInfoProxy currentUser;
+	private UserInfoDetailsProxy userDetails;
+	private List<MessagesProxy> messages= new ArrayList<MessagesProxy>();
 	
+
 
 
 	public MessagesActivity(ClientFactory clientFactory) {
@@ -26,13 +44,42 @@ public class MessagesActivity extends AbstractActivity implements MessagesView.P
 	}
 
 	private void bind() {
+		eventBus.addHandler(SendLoginEvent.TYPE, new SendLoginEventHandler() {
+			@Override
+			public void onSendLogin(SendLoginEvent event) {
+				currentUser = event.getCurrentUser();
+
+			}
+		});
 		showMessages();
 	}
 
 
 	protected void showMessages() {
-		
-		
+		UserInfoDetailsRequest request = requestFactory.userInfoDetailsRequest();
+		Request<UserInfoDetailsProxy> createReq = request.findPassengerFromUser(currentUser.getId());
+
+		createReq.fire(new Receiver<UserInfoDetailsProxy>() {
+			@Override
+			public void onSuccess(UserInfoDetailsProxy response) {
+				userDetails=response;
+				for (String message : userDetails.getMessages()) {
+					MessagesRequest requestMessages = requestFactory.messagesRequest();
+					Request<MessagesProxy> createReqMessages=requestMessages.findMessages(message);
+					createReqMessages.fire(new Receiver<MessagesProxy>() {
+
+						@Override
+						public void onSuccess(MessagesProxy response) {
+							messages.add(response);
+							//TODO maintenant qu'on a les messages, les afficher
+						}
+						
+					});
+				}
+				
+			}
+		});
+
 	}
 
 	@Override

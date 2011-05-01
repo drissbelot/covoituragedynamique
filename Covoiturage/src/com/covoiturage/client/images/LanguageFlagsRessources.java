6d938fbe29e182942a22134bb68e08a2ec1b89d6
apsforps <apsforps@2af29f76-a27f-8fc1-9967-9f9aca168193>
2011-05-01 14:14:15 +0000

    Pas tout mélanger, pas la bonne idée

--- Covoiturage/src/com/covoiturage/client/images/LanguageFlagsRessources.java
+++ Covoiturage/src/com/covoiturage/client/images/LanguageFlagsRessources.java
@@ -20,14 +20,19 @@ public interface LanguageFlagsRessources extends ClientBundle {
 	@Source("com/covoiturage/client/images/flag_nl.png")
 	ImageResource flag_nl();
 
+	@Source("flag_ch_list.png")
 	ImageResource flag_ch_list();
 
+	@Source("flag_en_list.png")
 	ImageResource flag_en_list();
 
+	@Source("flag_fr_list.png")
 	ImageResource flag_fr_list();
 
+	@Source("flag_it_list.png")
 	ImageResource flag_it_list();
 
+	@Source("flag_nl_list.png")
 	ImageResource flag_nl_list();
 
 }

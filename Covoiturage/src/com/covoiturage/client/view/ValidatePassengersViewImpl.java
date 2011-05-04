778231e849aa41006e48d63b1b3c1f3fb464b037
apsforps <apsforps@2af29f76-a27f-8fc1-9967-9f9aca168193>
2011-05-04 06:18:39 +0000

    uibinder (1)... on va essayer de faire du mvp avec des composite dans des composite dans des composite... ça fait un peu inception comme ça...

--- Covoiturage/src/com/covoiturage/client/view/ValidatePassengersViewImpl.java
+++ Covoiturage/src/com/covoiturage/client/view/ValidatePassengersViewImpl.java
@@ -1,44 +1,40 @@
 package com.covoiturage.client.view;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import com.covoiturage.client.activity.ValidatePassengersActivity;
 import com.extjs.gxt.ui.client.Style.SelectionMode;
 import com.extjs.gxt.ui.client.data.BaseModelData;
-import com.extjs.gxt.ui.client.store.ListStore;
+import com.extjs.gxt.ui.client.widget.ContentPanel;
 import com.extjs.gxt.ui.client.widget.grid.CheckBoxSelectionModel;
-import com.extjs.gxt.ui.client.widget.grid.ColumnConfig;
-import com.extjs.gxt.ui.client.widget.grid.ColumnModel;
 import com.extjs.gxt.ui.client.widget.grid.Grid;
 import com.extjs.gxt.ui.client.widget.grid.WidgetExpander;
 import com.extjs.gxt.ui.client.widget.grid.WidgetRowRenderer;
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.ui.Composite;
-import com.google.gwt.user.client.ui.VerticalPanel;
 import com.google.gwt.user.client.ui.Widget;
 
 public class ValidatePassengersViewImpl extends Composite implements
 		ValidatePassengersView {
 
+	interface MyUiBinder extends
+			UiBinder<ContentPanel, ValidatePassengersViewImpl> {
+	}
+
+	private static final MyUiBinder binder = GWT.create(MyUiBinder.class);
+
 	@SuppressWarnings("unused")
 	private Presenter presenter;
-
-	private final Grid<BaseModelData> listGrid;
-
-	private ColumnModel cm;
-	private ListStore<BaseModelData> store;
+	@UiField
+	Grid<BaseModelData> listGrid;
 
 	private WidgetExpander<BaseModelData> expander;
 	private ValidatePassengersExpander expanderWidget;
 
 	public ValidatePassengersViewImpl() {
 
-		VerticalPanel pan = new VerticalPanel();
 		CheckBoxSelectionModel<BaseModelData> check = new CheckBoxSelectionModel<BaseModelData>();
 		check.setSelectionMode(SelectionMode.MULTI);
-		List<ColumnConfig> configs = new ArrayList<ColumnConfig>();
-		configs.add(check.getColumn());
-
 		expander = new WidgetExpander<BaseModelData>(
 				new WidgetRowRenderer<BaseModelData>() {
 
@@ -49,29 +45,13 @@ public class ValidatePassengersViewImpl extends Composite implements
 					}
 
 				});
-		configs.add(expander);
-		ColumnConfig coordsOrigin = new ColumnConfig();
-		coordsOrigin.setId("origin");
-		coordsOrigin.setHeader("Origin");
-		configs.add(coordsOrigin);
-		ColumnConfig coordsDestination = new ColumnConfig();
-		coordsDestination.setId("destination");
-		coordsDestination.setHeader("Destination");
-		configs.add(coordsDestination);
-
-		cm = new ColumnModel(configs);
-
-		store = new ListStore<BaseModelData>();
-		listGrid = new Grid<BaseModelData>(store, cm);
+		initWidget(binder.createAndBindUi(this));
 		listGrid.addPlugin(expander);
-		listGrid.setWidth("300px");
-		listGrid.setHeight("300px");
+		listGrid.getColumnModel().getColumns().add(0, check.getColumn());
+		listGrid.getColumnModel().getColumns().add(1, expander);
 		listGrid.setSelectionModel(check);
 		listGrid.getView().setAutoFill(true);
 
-		pan.add(listGrid);
-
-		initWidget(pan);
 	}
 
 	@Override
